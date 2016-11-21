7aedc94e200c01acca10f4e2fdebdc9a49609e77
Ulrich Schmidt <admin@deruli.de>
2016-11-21 23:32:30 +0100

    tabelle korigiert

--- src/content/modules/database_browser/templates/single.php
+++ src/content/modules/database_browser/templates/single.php
@@ -2,35 +2,46 @@
 $acl = new ACL ();
 $table = $_GET ["single"];
 if ($acl->hasPermission ( "database_browser" ) and in_array ( $table, Database::getAllTables () )) {
-	$fields = Database::getColumnNames ( $table, false );
-	$result = Database::selectAll ( $table, array (), "", array (), false );
+	$query = Database::selectAll ( $table, array (), "", array (), false );
 	?>
-<form
-	action="<?php echo ModuleHelper::buildAdminURL("database_browser");?>"
-	method="get">
-	<input type="submit" value="<?php translate("back_to_list");?>">
+<form action="index.php" method="get">
+	<input type="hidden" name="action" value="module_settings"> <input
+		type="hidden" name="module" value="database_browser"> <input
+		type="submit" value="<?php translate("back_to_list");?>">
 </form>
-
-<table class="tablesorter">
-	<thead>
-		<tr>
-		<?php foreach($fields as $field){?>
-			<th><?php Template::escape($field);?></th>
-		<?php }?>
-		</tr>
-	</thead>
-	<tbody>
-	<?php
-	while ( $row = Database::fetchAssoc ( $result ) ) {
-		?>
-	<tr style="word-break: break-all;">
-	<?php foreach($row as $col){?>
-			<td><?php Template::escape($col);?></td>
-	<?php }?>
-		</tr>
-	<?php }?>
-	</tbody>
-</table>
+<div style="overflow: auto; width: 98%; height: 400px; margin: auto;">
+<?php
+	if ($query !== false and $query !== true) {
+		$fields_num = db_num_fields ( $query );
+		if ($fields_num) {
+			echo "<table class=\"tablesorter\"><thead><tr>";
+			// printing table headers
+			for($i = 0; $i < $fields_num; $i ++) {
+				$field = db_fetch_field ( $query );
+				echo "<th>{$field->name}</th>";
+			}
+			echo "</tr></thead><tbody>";
+			// printing table rows
+			while ( $row = db_fetch_row ( $query ) ) {
+				echo "<tr>";
+				
+				// $row is array... foreach( .. ) puts every element
+				// of $row to $cell variable
+				foreach ( $row as $cell ) {
+					$txt = htmlspecialchars ( $cell, ENT_COMPAT, "UTF-8" );
+					$txt = nl2br ( $txt );
+					echo "<td>$txt</td>";
+				}
+				
+				echo "</tr>
+";
+			}
+			
+			echo "</tbody></table></div>";
+		}
+	}
+	?>
+</div>
 <?php
 } else {
 	noperms ();
