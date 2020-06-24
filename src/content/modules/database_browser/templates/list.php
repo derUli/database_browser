6a7803a7410d18c964d9addb11d975b9214d76e7
Der Uli im Netz <admin@deruli.de>
2020-06-24 08:00:37 +0200

    update

--- src/content/modules/database_browser/templates/list.php
+++ src/content/modules/database_browser/templates/list.php
@@ -3,26 +3,27 @@ $acl = new ACL();
 if ($acl->hasPermission("database_browser")) {
     $tables = Database::getAllTables();
     ?>
-<table class="tablesorter">
-	<thead>
-		<tr>
-			<th>
-		<?php translate("name");?>
-		</th>
-		</tr>
-	</thead>
-	<tbody>
-	<?php foreach($tables as $table){?>
-	<tr>
-			<td><a name="field_<?php Template::escape($table);?>"></a><a
-				href="<?php echo ModuleHelper::buildAdminURL("database_browser", "single=$table")?>"
-				class="btn btn-default"><i class="fa fa-table" aria-hidden="true"></i> <?php Template::escape($table);?></a>
-			</td>
-		</tr>
-	<?php }?>
-	</tbody>
-</table>
-<?php
+    <table class="tablesorter">
+        <thead>
+            <tr>
+                <th>
+                    <?php translate("name"); ?>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($tables as $table) { ?>
+                <tr>
+                    <td>
+                        <a name="field_<?php Template::escape($table); ?>"></a><a
+                            href="<?php echo ModuleHelper::buildAdminURL("database_browser", "single=$table") ?>"
+                            class="btn btn-default"><i class="fa fa-table" aria-hidden="true"></i> <?php Template::escape($table); ?></a>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+    <?php
 } else {
     noperms();
 }
